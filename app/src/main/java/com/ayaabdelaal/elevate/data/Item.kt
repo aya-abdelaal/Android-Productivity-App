e973679eeff257f6bdfade2f4f13b5048d212f88
Aya <67323302+aya-abdelaal@users.noreply.github.com>
2022-08-22 16:53:17 +0200

    Initial commit

---
+++ app/src/main/java/com/ayaabdelaal/elevate/data/Item.kt
@@ -0,0 +1,17 @@
+package com.ayaabdelaal.elevate.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "item")
+data class Item(
+    @PrimaryKey(autoGenerate = true) private val id: Int = 0,
+    @ColumnInfo(name = "total_times") private val total :Int = 1,
+    @ColumnInfo(name = "color") private val color : Int,
+    @ColumnInfo(name = "done_times") private var done : Int = 0
+){
+    val percentageDone : Double
+    get() { return done.toDouble()/total.toDouble() }
+
+}
